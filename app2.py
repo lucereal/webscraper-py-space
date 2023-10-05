7f8ea6cc91469d846e8cc84df88aea4c3a17e853
David <lucerodavid@att.net>
2023-10-04 19:53:44 -0500

    now getting complete player stats from first page

--- app2.py
+++ app2.py
@@ -34,29 +34,54 @@ def run(playwright: Playwright) -> None:
     user = "lucerodavid1010@gmail.com"
     pwd = ""
     url = "https://www.espn.com/fantasy/"
-    statsUrl = "https://www.espn.com/nfl/stats"
+    statsUrl = "https://www.espn.com/nfl/stats/player"
     
     page.goto(statsUrl)
     try:
+        #user to select the option from the drop down 
+        #page.get_by_label('Choose a color').select_option('blue')
 
-        # login_button = page.wait_for_selector("text=Log In", timeout=10000)
-        # if login_button:
-        #     login_button.click()
-        #     page.wait_for_timeout(2000)
-        #     frame = page.frame(name="oneid-iframe")
-        #     if frame:
-        #         frame.locator("#InputIdentityFlowValue").fill(user)
-        #         frame.locator("#BtnSubmit").click()
-        #         page.wait_for_timeout(1000)
-        #         frame.locator("#InputPassword").fill(pwd)
-        #         frame.locator("#BtnSubmit").click()
-        #     else:
-        #         print("Could not find email address form")
-           
-        # else:
-        #     print("Login button not found.")
+        x = page.inner_html('.ResponsiveTable')
         
+        soup = BeautifulSoup(x, "html.parser")
+        tables = soup.findChildren('table')
+        playerTable = tables[0]
+        statsTable = tables[1]
         
+        players = playerTable.findChildren(['tr'])
+        
+        # foo = players[1].get('data-idx')
+        # print(foo)
+        player_dict = {}
+        # - get players from first table
+        for player in players[1:]:
+            pIndex = player.get('data-idx')
+            pd = player.findChildren('td')
+            pRank = pd[0].text
+            pName = pd[1].findChildren('a')[0].text
+            pTeam = pd[1].findChildren('span')[0].text
+            player_dict[pIndex] = [pRank,pName,pTeam]
+       
+        
+        # - get stats for each player from second table
+        stats = statsTable.findChildren('tr')
+        for stat in stats[1:]:          
+            sIndex = stat.get('data-idx')          
+            sdi = [i.text for i in stat.findChildren('td')]
+            for sdix in sdi:
+                player_dict[sIndex].append(sdix)
+
+        
+            
+            #data-idx="0"
+        # player1Data = players[1].findChildren(['td'])
+        # player1Rank = player1Data[0].text
+        # player1Name = player1Data[1].findChildren(['a'])[0].text
+        # player1Team = player1Data[1].findChildren(['span'])[0].text
+        # print(player1Rank)
+        # print(player1Name)
+        # print(player1Team)
+    
         
     except Exception as error:
         print("An exception occurred:", error) # An exception occurred: division by zero
@@ -74,4 +99,25 @@ def run(playwright: Playwright) -> None:
 with sync_playwright() as playwright:
     run(playwright)
 
+#url = "https://www.espn.com/fantasy/"
+#select login then enter credentials
+# login_button = page.wait_for_selector("text=Log In", timeout=10000)
+# if login_button:
+#     login_button.click()
+#     page.wait_for_timeout(2000)
+#     frame = page.frame(name="oneid-iframe")
+#     if frame:
+#         frame.locator("#InputIdentityFlowValue").fill(user)
+#         frame.locator("#BtnSubmit").click()
+#         page.wait_for_timeout(1000)
+#         frame.locator("#InputPassword").fill(pwd)
+#         frame.locator("#BtnSubmit").click()
+#     else:
+#         print("Could not find email address form")
 
+# else:
+#     print("Login button not found.")
+
+# expect(page.get_by_role("heading", name="Sign up")).to_be_visible()
+
+# page.get_by_role("checkbox", name="Subscribe").check()
